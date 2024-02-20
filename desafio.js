5ec7f7d57b3b2113d6becdcc2d81b68c01e3381d
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2024-02-20 15:16:34 -0300

    Função separarEndereco
    Criando a função para separar os nomes dos números da rua

--- desafio.js
+++ desafio.js
@@ -1,3 +1,18 @@
+function separarEndereco(endereco) {
+    // Regex: Expressão regular que filtra o nome e o número da rua
+    // Nome da rua ([^\d]+): Sequencia de caracteres que não sejam digitos
+    // Número da rua (\d+\w*): Sequencia de digitos seguida por um alfanumerico para o endereço Cambuí
+    const regex = /([^\d]+) (\d+\w*)/;
+    // Passando o regex na string de endereço recebida na função
+    const aux = endereco.match(regex);
+    // Retornando o nome e o número da rua do array aux
+    if (aux) {
+        const rua = aux[1].trim();
+        const numero = aux[2].trim();
+        return [rua, numero];
+    } else return null;
+}
+
 //Endereços teste
 const enderecos = [
     "Miritiba 339",
