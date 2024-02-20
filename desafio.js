d8cb8ac4ced70ad99b9f09f7efa503aa9de545c8
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2024-02-20 15:07:58 -0300

    Endereços de Teste
    Primeiros endereços a serem testados.

---
+++ desafio.js
@@ -0,0 +1,7 @@
+//Endereços teste
+const enderecos = [
+    "Miritiba 339",
+    "Babaçu 500",
+    "Cambuí 804B"
+    //No PDF a entrada e saída do Cambuí está diferente, irei utilizar este exemplo da entrada
+];
