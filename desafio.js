fa027caaa0a7eb8062ffaa4b65b8ab60f4191cef
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2024-02-20 15:37:07 -0300

    Att/Função separarEndereco/regex
    Atualização no regex da função separarEndereco para capturar os novos números das ruas

--- desafio.js
+++ desafio.js
@@ -1,5 +1,7 @@
+// Para os casos mais complicados precisei apenas alterar a expressão regular para capturar os numeros das ruas
 function separarEndereco(endereco) {
-    const regex = /([^\d]+) (\d+\w*)/;
+    // Número da rua (\d+\w*\s*\w*): Agora permite uma sequencia de digitos seguida por um ou mais espaços seguida por um ou mais alfanumericos
+    const regex = /([^\d]+) (\d+\s*\w*)/;
     const aux = endereco.match(regex);
     if (aux) {
         const rua = aux[1].trim();
