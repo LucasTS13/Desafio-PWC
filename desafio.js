8e9db1427431ce7c475aaefb957ccdf2a4341523
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2024-02-20 16:28:41 -0300

    Att/separarEndereco
    Atualização da função separarEndereco para os casos complexos.

--- desafio.js
+++ desafio.js
@@ -1,11 +1,44 @@
+// Para os casos Complexos tive que separar as strings em 3 casos dentro da função.
 function separarEndereco(endereco) {
-    const regex = /([^\d]+) (\d+\s*\w*)/;
-    const aux = endereco.match(regex);
-    if (aux) {
-        const rua = aux[1].trim();
-        const numero = aux[2].trim();
-        return [rua, numero];
-    } else return null;
+    // Caso 1: Quando a string de endereço vem com o número da rua antes do nome.
+    // O if verifica se a string começa com um número.
+    if (endereco[0].match(/\d/)) {
+        // Remove a vírgula das strings.
+        endereco = endereco.replace(/[^\s\wÀ-ÿ]/gu, "");
+        // O regex filtra uma sequencia de digitos e uma sequencia de não digitos
+        const regex = /(\d+) ([^\d]+)/;
+        const aux = endereco.match(regex);
+        if (aux) {
+            // Inverti a posição no vetor auxiliar pois o número vem a frente do nome neste caso.
+            const rua = aux[2].trim();
+            const numero = aux[1].trim();
+            return [rua, numero];
+        } else return null;
+    // Caso 2: Quando o nome da rua possui um número.
+    // O if verifica se a string começa com não digitos e verifica se o padrão possui letras seguidas de números duas vezes, para o caso em que o nome da rua é composto de um número.
+    } else if (endereco[0].match(/[^\d]/) && endereco.match(/\w+\d+\w+\d+/)) {
+        // O regex filtra uma sequencia de não digitos e uma sequencia de digitos como nome da rua e também uma sequencia de não digitos e uma sequencia de digitos como número da rua.
+        const regex = /([^\d]+\d+) ([^\d]+\d+)/;
+        // Restante do código identico ao das versões anteriores.
+        const aux = endereco.match(regex);
+        if (aux) {
+            const rua = aux[1].trim();
+            const numero = aux[2].trim();
+            return [rua, numero];
+        } else return null;
+    // Caso 3: Código reaproveitado das versões anteriores, apenas adicionei uma linha para remover a virgula antes de comparar no match.
+    } else {
+        // Remove a vírgula das strings.
+        endereco = endereco.replace(/[^\s\wÀ-ÿ]/gu, "");
+        // Restante do código reaproveitado das versões anteriores.
+        const regex = /([^\d]+) (\d+\s*\w*)/;
+        const aux = endereco.match(regex);
+        if (aux) {
+            const rua = aux[1].trim();
+            const numero = aux[2].trim();
+            return [rua, numero];
+        } else return null;
+    }
 }
 
 //Novos endereços teste
